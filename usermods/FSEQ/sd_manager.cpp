8fbf574f35a858ea53fd82afcc4a01c716318940
Danit2 <71522810+Danit2@users.noreply.github.com>
2026-02-23 22:16:03 +0100

    Update sd_manager.cpp

--- usermods/FSEQ/sd_manager.cpp
+++ usermods/FSEQ/sd_manager.cpp
@@ -18,4 +18,31 @@ bool SDManager::begin() {
 
 void SDManager::end() { SD_ADAPTER.end(); }
 
+String SDManager::listFiles(const char *dirname) {
+  String result = "";
+  File root = SD_ADAPTER.open(dirname);
+  if (!root) {
+    result += "<li>Failed to open directory: ";
+    result += dirname;
+    result += "</li>";
+    return result;
+  }
+  if (!root.isDirectory()) {
+    result += "<li>Not a directory: ";
+    result += dirname;
+    result += "</li>";
+    return result;
+  }
+  File file = root.openNextFile();
+  while (file) {
+    result += "<li>";
+    result += file.name();
+    result += " (" + String(file.size()) + " bytes)</li>";
+    file.close();
+    file = root.openNextFile();
+  }
+  root.close();
+  return result;
+}
+
 bool SDManager::deleteFile(const char *path) { return SD_ADAPTER.remove(path); }
